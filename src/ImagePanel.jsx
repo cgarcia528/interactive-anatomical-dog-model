672bfb892e04e143be3682ac069c52acfaf1947a
Clarissa <cgarcia528@unm.edu>
2022-11-02 09:53:51 -0600

    Changed and edited imports to work with image panel using an image and json import

--- src/ImagePanel.jsx
+++ src/ImagePanel.jsx
@@ -1,60 +1,49 @@
 import React, { Component } from "react";
 import StartingPic from "./images/startingpic.png";
-import layer6 from "./images/layer6.png";
-import musclesJson from "./muscles.json";
-import DOGSVG from "./DOGSVG";
+import "./App.css";
+import imageImport, { SixHundredsPic } from './images/imageImport';
+import jsonImport, { SixHundreds } from './json/jsonImport';
+
+function handleHover(e) {
+  const found = SixHundreds.find((element) => {
+    return element.id === e.target.id;
+  });
+  console.log(found.bodyName);
+  console.log(found.action);
+}
 
 class ImagePanel extends Component {
   render() {
     return (
-      <DOGSVG></DOGSVG>
-      // <div className="image-container">
-      //   <img
-      //     src={StartingPic}
-      //     className="visible starting-img"
-      //     alt="string"
-      //   ></img>
-
-      //   {this.props.imageNameArray.map((object, index) => (
-      //     <img
-      //       key={index.toString()}
-      //       src={
-      //         this.props.buttonsArray[index]
-      //           ? this.props.imageNameArray[index].imageName
-      //           : undefined
-      //       }
-      //       className="opacity-50 img layered-img"
-      //     ></img>
-      //   ))}
-      // </div>
-      // <div className="image-container">
-      //   <svg
-      //     version="1.1"
-      //     viewBox="0 0 4204 3294"
-      //     xmlns="http://www.w3.org/2000/svg"
-      //     xmlnsXlink="http://www.w3.org/1999/xlink"
-      //   >
-      //     <image
-      //       className="opacity-50 img layered-img"
-      //       width="4204"
-      //       height="3294"
-      //       preserveAspectRatio="none"
-      //       xlinkHref={layer6}
-      //     />
-      //     <g>
-      //       {musclesJson.map((el) => (
-      //         <g key={el.id} className="full-group">
-      //           <path
-      //             id={el.id}
-      //             key={el.id}
-      //             d={el.path}
-      //             onMouseEnter={handleHover}
-      //           />
-      //         </g>
-      //       ))}
-      //     </g>
-      //   </svg>
-      // </div>
+      <div className="image-container">
+      <svg
+        version="1.1"
+        viewBox="0 0 4204 3294"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        // className="image-container"
+      >
+        <image
+          className="opacity-50 img layered-img"
+          // width="4204"
+          // height="3294"
+          preserveAspectRatio="none"
+          xlinkHref={SixHundredsPic}
+        />
+        <g>
+          {SixHundreds.map((el) => (
+            <g key={el.id} className="full-group">
+              <path
+                id={el.id}
+                key={el.id}
+                d={el.path}
+                onMouseEnter={handleHover}
+              />
+            </g>
+          ))}
+        </g>
+      </svg>
+    </div>
     );
   }
 }
