49823664a06946fbef540ddcf6fc7d4466258ace
Clarissa <cgarcia528@unm.edu>
2022-11-02 23:15:46 -0600

    Removed unnecesary new lines in ImagePanel. Added more information to json entries including names and actions for anatomy hotspots.

--- src/ImagePanel.jsx
+++ src/ImagePanel.jsx
@@ -3,43 +3,39 @@ import StartingPic from "./images/startingpic.png";
 import "./App.css";
 
 class ImagePanel extends Component {
-  render() {  
-    const { 
-      mostRecentContainer,
-      onEnter,
-      onLeave
-  } = this.props;
+  render() {
+    const { mostRecentContainer, onEnter, onLeave } = this.props;
     return (
       <div className="image-container">
-      <svg
-        version="1.1"
-        viewBox="0 0 4204 3294"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        // className="image-container"
-      >
-        <image
-          className="opacity-50 img layered-img"
-          // width="4204"
-          // height="3294"
-          preserveAspectRatio="none"
-          xlinkHref={mostRecentContainer.imageName}
-        />
-        <g>
-          {mostRecentContainer.textField[0].array.map((el) => (
-            <g key={el.id} className="full-group">
-              <path
-                id={el.id}
-                key={el.id}
-                d={el.path}
-                onMouseEnter={(e) => onEnter(e)}
-                onMouseLeave={onLeave}
-              />
-            </g>
-          ))}
-        </g>
-      </svg>
-    </div>
+        <svg
+          version="1.1"
+          viewBox="0 0 4204 3294"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          // className="image-container"
+        >
+          <image
+            className="opacity-50 img layered-img"
+            // width="4204"
+            // height="3294"
+            preserveAspectRatio="none"
+            xlinkHref={mostRecentContainer.imageName}
+          />
+          <g>
+            {mostRecentContainer.textField[0].array.map((el) => (
+              <g key={el.id} className="full-group">
+                <path
+                  id={el.id}
+                  key={el.id}
+                  d={el.path}
+                  onMouseEnter={(e) => onEnter(e)}
+                  onMouseLeave={onLeave}
+                />
+              </g>
+            ))}
+          </g>
+        </svg>
+      </div>
     );
   }
 }
